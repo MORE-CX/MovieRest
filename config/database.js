78cad6a520ed1d257c36c35fde8e97d3ea5820f8
MauroMCX <sosamauro1@gmail.com>
2019-05-14 16:55:53 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,5 +2,5 @@
 module.exports = {
 	// la url par conectarse con la base de datos
 	//url : 'mongodb://localhost:27017/peliculas'
-	url : 'mongodb://mcxprueba:mcxprueba1@ds141812.mlab.com:41812/pruebamcx'
+	url : 'mongodb://*****:*****@ds141812.mlab.com:41812/pruebamcx'
 }
